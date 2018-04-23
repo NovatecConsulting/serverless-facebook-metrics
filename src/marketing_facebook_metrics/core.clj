cbb7910cfbd51f701bcc4f38b281a1b04f5951c7
Timo Freiberg <timo.freiberg@gmail.com>
2018-04-23 21:37:40 +0200

    Use clj-http as client library

--- src/marketing_facebook_metrics/core.clj
+++ src/marketing_facebook_metrics/core.clj
@@ -1,5 +1,6 @@
 (ns marketing-facebook-metrics.core
-  (:require [uswitch.lambada.core :refer [deflambdafn]]))
+  (:require [uswitch.lambada.core :refer [deflambdafn]]
+            [clj-http.client :as client]))
 
 (deflambdafn de.novatec.MarketingFacebookMetrics
   [in out ctx]
