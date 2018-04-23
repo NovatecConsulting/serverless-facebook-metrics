aceb3afe5543469e44ce90ff8439fce872581530
Timo Freiberg <timo.freiberg@gmail.com>
2018-04-23 21:37:22 +0200

    Add environ and environment variables

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [uswitch/lambada "0.1.2"]
-                 [clj-http "3.8.0"]]
-  :profiles {:uberjar {:aot :all}}
+                 [clj-http "3.8.0"]
+                 [environ "1.1.0"]]
+  :profiles {:uberjar {:aot :all}
+             :dev [:dev-env]}
   :uberjar-name "marketing-facebook-metrics.jar")
