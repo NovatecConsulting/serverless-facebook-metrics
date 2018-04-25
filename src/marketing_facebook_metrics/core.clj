c066aad29093aa0a8f4e589f9939b9db654943c3
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-04-25 22:39:09 +0200

    Switch to user token

--- src/marketing_facebook_metrics/core.clj
+++ src/marketing_facebook_metrics/core.clj
@@ -1,5 +1,5 @@
 (ns marketing-facebook-metrics.core
-  (:require #_[uswitch.lambada.core :refer [deflambdafn]]
+  (:require [uswitch.lambada.core :refer [deflambdafn]]
             [clj-http.client :as client]
             [environ.core :refer [env]]))
 
@@ -11,24 +11,10 @@
   [item-id]
   (client/get
    (str "https://graph.facebook.com/v2.11/" item-id)
-   {:query-params {"access_token" (:app-token env)}}))
+   {:query-params {"access_token" (:user-token env)}
+    :debug true}))
 
-(defmacro deflambdafn
-  "Create a named class that can be invoked as a AWS Lambda function."
-  [name args & body]
-  (assert (= (count args) 3) "lambda function must have exactly three args")
-  (let [prefix (gensym)
-        handleRequestMethod (symbol (str prefix "handleRequest"))]
-    `(do
-       (gen-class
-        :name ~name
-        :prefix ~prefix
-        :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
-       (defn ~handleRequestMethod
-         ~(into ['this] args)
-         ~@body))))
-
-(deflambdafn de.novatec.MarketingFacebookMetrics
+(defn handle-lambda
   [in out ctx]
   (println "Getting general data for the NovaTec Holding facebook page.")
   (let [page-data (try
@@ -38,3 +24,7 @@
                            (clojure.pprint/pprint
                             (ex-data e)))))]
     (spit out page-data)))
+
+(deflambdafn de.novatec.MarketingFacebookMetrics
+  [in out ctx]
+  (handle-lambda in out ctx))
