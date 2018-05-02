d51990c235de7d3d31b9866c7e7e1d1f005f3321
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-05-02 16:44:43 +0200

    Inline new deflambdafn macro

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/nt-ca-aqe/marketing-facebook-metrics"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [uswitch/lambada "0.1.2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [lambada "1.0.3"]
                  [clj-http "3.8.0"]
                  [environ "1.1.0"]
                  [cheshire "5.8.0"]
--- src/marketing_facebook_metrics/core.clj
+++ src/marketing_facebook_metrics/core.clj
@@ -1,14 +1,11 @@
 (ns marketing-facebook-metrics.core
-  (:require [uswitch.lambada.core :refer [deflambdafn]]
+  (:require #_[lambada.core :refer [deflambdafn]]
             [clj-http.client :as client]
             [environ.core :refer [env]]
             [cheshire.core :as json]
             [java-time :as time]
             [clojure.java.io :as io]
-            [clojure.pprint :refer [pprint]])
-  (:gen-class
-   :name de.novatec.MarketingFacebookMetrics
-   :methods [[handleRequest [String Object] String]]))
+            [clojure.pprint :refer [pprint]]))
 
 (defn parse-timestamp
   [post]
@@ -51,12 +48,44 @@
       ;; (json/generate-stream response (io/writer out))
       (json/generate-string response))))
 
-(defn -handleRequest
-  [in ctx]
-  (handle-lambda in ctx))
+(defmacro deflambdafn
+  "Create a named class that can be invoked as a AWS Lambda function.
+  `class-name` must be a symbol defining a fully qualified class name.
+  `fun-name` must be a symbol which will be the name of the defined function.
+  `args` must be a vector of 2 arguments.
+  `body` is the body that will be executed by your lambda function.
 
-#_(macroexpand '(deflambdafn de.novatec.MarketingFacebookMetrics
-   [in out ctx]
-   (handle-lambda in out ctx)))
-#_(do (clojure.core/gen-class :name de.novatec.MarketingFacebookMetrics :prefix G__20464 :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler]) (clojure.core/defn G__20464handleRequest [this in out ctx] (handle-lambda in out ctx)))
+  Arguments:
+  The 2 arguments are
+  1. The input argument to your lambda function
+  2. The context of your lambda function invocation, supplied by the AWS infrastructure. This is a `com.amazonaws.services.lambda.runtime.Context`.
+
+  The return value of your handler function will be used as the response.
+
+  Example:
+  (deflambdafn com.mycompany.somedomain.MyFunctionHandler handleRequest
+    [in ctx]
+    (println \"Request:\" in)
+    (handle-request in ctx))
+
+  "
+  [class-name fun-name args & body]
+  (assert (symbol? class-name) "Lambda handler class name must be a symbol")
+  (assert (symbol? fun-name) "Lambda handler function name must be a symbol")
+  (assert (and (vector? args)
+               (= 2 (count args)))
+          "Lambda handler args must be a vector of 2 elements")
+  (let [method-name (symbol (str "-" fun-name))]
+    `(do
+       (gen-class
+        :name ~class-name
+        :methods [[~fun-name [Object Object] Object]])
+       (defn ~fun-name ~args ~@body)
+       (defn ~method-name
+         ~(into ['this] args)
+         (~fun-name ~@args)))))
+
+(deflambdafn de.novatec.MarketingFacebookMetrics
+   [in ctx]
+   (handle-lambda in ctx))
 
